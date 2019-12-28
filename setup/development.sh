81193f17ed1267fb649c0bb7176004e1bca40976
Max Goldman <maxg@mit.edu>
2019-12-28 17:46:09 -0500

    Add missing `zip` in VM

--- setup/development.sh
+++ setup/development.sh
@@ -8,7 +8,7 @@ wget -qO - https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
 apt-get update
 
 # Apt packages
-apt-get install -y python-pip nodejs build-essential
+apt-get install -y python-pip nodejs build-essential zip
 
 # AWS CLI
 pip install awscli --upgrade
